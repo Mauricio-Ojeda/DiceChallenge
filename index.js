fa290c0dd900a8dba5ed824c6d316b066fe141a4
Mauricio Ojeda <ojedam150@gmail.com>
2021-04-13 23:06:46 -0300

    update javaScript and add button to throw dices

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-let randomNumber1 = Math.floor( Math.random() * 6) + 1;
-let randomNumber2 = Math.floor( Math.random() * 6) + 1;
+let randomNumberPlayer1 = Math.floor(Math.random() * 6) + 1;
+let randomNumberPlayer2 = Math.floor(Math.random() * 6) + 1;
+const title = document.querySelector('h1'),
+    diceImg = document.querySelectorAll('img'),
+    throwDice = document.querySelector('#reload');
 
-document.querySelector('img').setAttribute("src", "images/dice" + randomNumber1 +".png");
+// dice image to player 1
+diceImg[0].setAttribute("src", "images/dice" + randomNumberPlayer1 + ".png");
 
-document.querySelectorAll('img')[1].setAttribute("src", "images/dice" + randomNumber2 +".png");
+//dice image to player 2
+diceImg[1].setAttribute("src", "images/dice" + randomNumberPlayer2 + ".png");
 
-if (randomNumber1 > randomNumber2) document.querySelector('h1').innerHTML = "🏆Player 1 Wins!";
-else if (randomNumber1 < randomNumber2) document.querySelector('h1').innerHTML = "🏆Player 2 Wins!";
-else document.querySelector('h1').innerHTML = "🎈Draw!";
+// search winner
+let winner = (randomNumberPlayer1 > randomNumberPlayer2) ? "🏆Player 1 Wins!" :
+    (randomNumberPlayer1 < randomNumberPlayer2) ? "🏆Player 2 Wins!" : "🎈Draw!"
+
+// show winner in title
+title.innerText = winner;
+
+// throw dice
+reload.addEventListener('click', () => {
+    location.reload();
+});
